3d371afcfda1ba9578ff1a28e2b3eaada4bdf006
muellers <stefan.mueller@it.nrw.de>
2012-07-13 10:36:28 +0200

    Comments

--- holodeck/modules/holodeck-logging/src/main/java/org/holodeck/logging/appender/HolodeckAppender.java
+++ holodeck/modules/holodeck-logging/src/main/java/org/holodeck/logging/appender/HolodeckAppender.java
@@ -4,50 +4,48 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
-import org.holodeck.logging.level.Message;
 import org.holodeck.logging.module.Constants;
 import org.holodeck.logging.module.DbStore;
 import org.holodeck.logging.persistent.LoggerEvent;
 import org.holodeck.logging.persistent.LoggerMessage;
 import org.holodeck.logging.persistent.MessageInfo;
 
-
+/**
+ * A custom appender for logging into a database via hibernate
+ * 
+ * @author Stefan Mueller
+ * @author Tim Nowosadtko
+ * @date 07-13-2012
+ */
 public class HolodeckAppender extends AppenderSkeleton implements Appender{
 
 	private static final Logger log = Logger.getLogger(HolodeckAppender.class);
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public boolean requiresLayout() {
-		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+    /**
+     * @param LoggingEvent
+     * This method stores custom and standard logevents into the database. In case of the cuastom logevent MESSAGE it expects a message of type MessageInfo
+     */
 	protected void append(LoggingEvent event) {
 		
-		// TODO Auto-generated method stub
-//		log.info("Create DBStore");
-		System.out.println("test1");
-//		Constants.loadDataStore();
 		if(Constants.store == null)Constants.store=new DbStore();
 		DbStore dbs=Constants.store;
-//		System.out.println("dbs"+dbs.toString());
-		System.out.println(event.getMessage().getClass().getName());
 		if(event.getMessage() instanceof MessageInfo){
 			log.info("Logelement: Message");
-			log.info("Messageclass: "+event.getMessage().toString());
 			LoggerMessage lm = new LoggerMessage((MessageInfo)event.getMessage());
 			if(dbs!=null)dbs.save(lm);
-			System.out.println("Message gespeichert.");
 			
 		}
 		else{
 			LoggerEvent le = new LoggerEvent(event);
 			if(dbs!=null)dbs.save(le);
-			System.out.println("Event gespeichert.");
 		}
 		
 	}
