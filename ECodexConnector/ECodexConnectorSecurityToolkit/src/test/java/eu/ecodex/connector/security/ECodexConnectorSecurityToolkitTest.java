3ad5dd08745b4985d5211562094dc32fd45b0b45
riedlerb <riederb@extern.brz.gv.at>
2013-04-10 10:51:22 +0200

    bugfixes and release-notes-1.0

--- ECodexConnector/ECodexConnectorSecurityToolkit/src/test/java/eu/ecodex/connector/security/ECodexConnectorSecurityToolkitTest.java
+++ ECodexConnector/ECodexConnectorSecurityToolkit/src/test/java/eu/ecodex/connector/security/ECodexConnectorSecurityToolkitTest.java
@@ -1,18 +1,24 @@
 package eu.ecodex.connector.security;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
+import eu.ecodex.connector.common.message.Message;
+import eu.ecodex.connector.common.message.MessageAttachment;
+import eu.ecodex.connector.common.message.MessageContent;
+import eu.ecodex.connector.common.message.MessageDetails;
 import eu.ecodex.connector.security.container.ECodexSecurityContainer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -30,55 +36,65 @@ public class ECodexConnectorSecurityToolkitTest {
 
         System.out.println("Test skipped!");
 
-        // MessageDetails details = new MessageDetails();
-        //
-        // MessageContent content = new MessageContent();
-        //
-        // try {
-        // content.setPdfDocument(getRessource("siginfos-1_p12-acro_pers1-acro_pers2-acro.pdf"));
-        // // content.setPdfDocument(getRessource("content.pdf"));
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        //
-        // Message message = new Message(details, content);
-        //
-        // try {
-        // securityContainer.createContainer(message);
-        //
-        // } catch (Exception e) {
-        // LOGGER.error(e.getMessage());
-        // e.printStackTrace();
-        // }
-        //
-        // Assert.notEmpty(message.getAttachments());
-        //
-        // securityContainer.recieveContainerContents(message);
-        //
-        // for (MessageAttachment attachment : message.getAttachments()) {
-        // if (attachment.getName().equals("Token.xml")) {
-        // try {
-        // FileOutputStream fos = new FileOutputStream("/result_token.xml");
-        // fos.write(attachment.getAttachment());
-        // fos.close();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // } else if (attachment.getName().equals("Token.pdf")) {
-        // try {
-        // FileOutputStream fos = new FileOutputStream("/result_token.pdf");
-        // fos.write(attachment.getAttachment());
-        // fos.close();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // }
-        // }
+        MessageDetails details = new MessageDetails();
+
+        MessageContent content = new MessageContent();
+
+        try {
+            content.setPdfDocument(getRessource("siginfos-1_p12-acro_pers1-acro_pers2-acro.pdf"));
+            // content.setPdfDocument(getRessource("content.pdf"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Message message = new Message(details, content);
+
+        try {
+            securityContainer.createContainer(message);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+        Assert.notEmpty(message.getAttachments());
+
+        securityContainer.recieveContainerContents(message);
+
+        String dir = System.getProperty("user.dir") + "/src/test/resources/";
+        for (MessageAttachment attachment : message.getAttachments()) {
+
+            if (attachment.getName().equals("Token.xml")) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(dir + "result_token.xml");
+                    fos.write(attachment.getAttachment());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (attachment.getName().equals("Token.pdf")) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(dir + "result_token.pdf");
+                    fos.write(attachment.getAttachment());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
     protected static byte[] getRessource(final String name) throws IOException {
-        final InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
-        return IOUtils.toByteArray(inputStream);
+        String dir = System.getProperty("user.dir");
+
+        File file = new File(dir + "/src/test/resources/" + name);
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fileInputStream.read(data);
+        fileInputStream.close();
+
+        return data;
     }
 }
