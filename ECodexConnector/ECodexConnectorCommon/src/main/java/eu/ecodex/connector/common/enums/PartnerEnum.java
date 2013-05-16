09f789d4d62fc41ab9b8a06ef19fb9c91ffbbe6a
muellers <stefan.mueller@it.nrw.de>
2013-05-16 13:49:03 +0200

    List of Partners (PartnerEnum) updated. Version changed to 1.3.1. Keystorepath in test.properties changed
    Following partners added:
    GR, EU, BE, TR, PL, FR, NL, HU, PT, MT, LT, SE, GB

--- ECodexConnector/ECodexConnectorCommon/src/main/java/eu/ecodex/connector/common/enums/PartnerEnum.java
+++ ECodexConnector/ECodexConnectorCommon/src/main/java/eu/ecodex/connector/common/enums/PartnerEnum.java
@@ -1,7 +1,7 @@
 package eu.ecodex.connector.common.enums;
 
 public enum PartnerEnum {
-    AT("AT", "GW"), DE("DE", "GW"), IT("IT", "GW"), ES("ES", "GW"), EE("EE", "GW");
+    AT("AT", "GW"), DE("DE", "GW"), IT("IT", "GW"), ES("ES", "GW"), EE("EE", "GW"), EU("EU", "GW"), CZ("CZ", "GW"), GR("GR", "GW"), BE("BE", "GW"), TR("TR", "GW"), PL("PL", "GW"), FR("FR", "GW"), NL("NL", "GW"), HU("HU", "GW"), PT("PT", "GW"), MT("MT", "GW"), LT("LT", "GW"), SE("SE", "GW"), GB("GB", "GW");
 
     private final String name;
     private final String role;
