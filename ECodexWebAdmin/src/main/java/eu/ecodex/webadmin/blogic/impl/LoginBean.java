83dd949556e61a2577627d6b3e1e92e5c71bf3fe
michalim <michalim@PC78248.brzg.local>
2014-05-09 17:39:57 +0200

    Login changes - moved to right template

--- ECodexWebAdmin/src/main/java/eu/ecodex/webadmin/blogic/impl/LoginBean.java
+++ ECodexWebAdmin/src/main/java/eu/ecodex/webadmin/blogic/impl/LoginBean.java
@@ -14,6 +14,34 @@ public class LoginBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private String password;
     private String message, uname;
+    private boolean loggedIn = false;
+
+    public String loginProject() {
+        boolean result = UserDao.login(uname, password);
+        if (result) {
+            // get Http Session and store username
+            HttpSession session = Util.getSession();
+            session.setAttribute("username", uname);
+            loggedIn = true;
+            return "home";
+        } else {
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalid Login!", "Please Try Again!"));
+
+            // invalidate session, and redirect to other pages
+
+            // message = "Invalid Login. Please Try Again!";
+            loggedIn = false;
+            return "login";
+        }
+    }
+
+    public String logout() {
+        HttpSession session = Util.getSession();
+        session.invalidate();
+        return "login";
+    }
 
     public String getMessage() {
         return message;
@@ -39,29 +67,11 @@ public class LoginBean implements Serializable {
         this.uname = uname;
     }
 
-    public String loginProject() {
-        boolean result = UserDao.login(uname, password);
-        if (result) {
-            // get Http Session and store username
-            HttpSession session = Util.getSession();
-            session.setAttribute("username", uname);
-
-            return "home";
-        } else {
-
-            FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalid Login!", "Please Try Again!"));
-
-            // invalidate session, and redirect to other pages
-
-            // message = "Invalid Login. Please Try Again!";
-            return "login";
-        }
+    public boolean isLoggedIn() {
+        return loggedIn;
     }
 
-    public String logout() {
-        HttpSession session = Util.getSession();
-        session.invalidate();
-        return "login";
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
     }
 }
