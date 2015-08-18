f08d57e6cd3a58d0832783d5321ba96ffa59f803
riederb <riederb@PC64669.brzg.local>
2015-08-18 10:46:15 +0200

    git issues again

--- domibusConnector/domibusConnectorController/src/main/java/eu/ecodex/connector/controller/service/EvidenceService.java
+++
@@ -1,9 +0,0 @@
-package eu.ecodex.connector.controller.service;
-
-import eu.domibus.connector.common.message.Message;
-import eu.ecodex.connector.controller.exception.ECodexConnectorControllerException;
-
-public interface EvidenceService {
-
-    void handleEvidence(Message confirmationMessage) throws ECodexConnectorControllerException;
-}
