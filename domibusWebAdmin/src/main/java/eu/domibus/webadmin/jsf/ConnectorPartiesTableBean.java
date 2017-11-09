f58e9f5e58f1c8d96a946ae1ffa899db045cdb13
Stephan <stephan.spindler@activesolution.com>
2017-11-09 17:09:40 +0100

    connectionParty UI
    edit connection party, delete connection party, create connection party
    working in ui

--- domibusWebAdmin/src/main/java/eu/domibus/webadmin/jsf/ConnectorPartiesTableBean.java
+++ domibusWebAdmin/src/main/java/eu/domibus/webadmin/jsf/ConnectorPartiesTableBean.java
@@ -15,19 +15,57 @@ public class ConnectorPartiesTableBean {
 
 	private final static Logger LOG = LoggerFactory.getLogger(ConnectorPartiesTableBean.class);
 	
+	/**
+	 * backend
+	 */
 	private IConnectorPModeSupport pModeSupport;
 	
+	/**
+	 * list of parties - retrieved from backend
+	 */
 	private List<DomibusConnectorParty> partyList;
+	
+	/**
+	 * list of selected parties
+	 */
 	private List<DomibusConnectorParty> selectedParties = new ArrayList<>();
+	
+	/**
+	 * Holds the party which is being edited by the createEditDialog
+	 */
+	private DomibusConnectorParty party;
+
+	/**
+	 * handles if a new party is created (true) or
+	 * if a party is being edited
+	 */
+	private boolean createNewPartyMode;
+	
+	/**
+	 * holds the editDialogTitle text 
+	 */
+	private String editDialogTitle = "";
+		
+	/**
+	 * holds the text of the confirmButton at the editDialog
+	 */
+	private String editDialogConfirmButtonText = "";
+	
 	
 	public void init() {
 		setPartyList(this.pModeSupport.getPartyList());
 	}
 	
+	
+	/*
+	 * SETTER + GETTER	 
+	 */
+	
 	public IConnectorPModeSupport getpModeSupport() {
 		return pModeSupport;
 	}
 
+	
 	public void setpModeSupport(IConnectorPModeSupport pModeSupport) {
 		this.pModeSupport = pModeSupport;
 	}
@@ -42,33 +80,101 @@ public class ConnectorPartiesTableBean {
 		this.partyList = partyList;
 	}
 
+	
 	public List<DomibusConnectorParty> getSelectedParties() {
 		return selectedParties;
 	}
 
+	
+	public DomibusConnectorParty getParty() {
+		return party;
+	}
+
+	
+	public void setParty(DomibusConnectorParty party) {
+		this.party = party;
+	}
+
+	
+	public String getEditDialogTitle() {
+		return editDialogTitle;
+	}
+
+	
+	public void setEditDialogTitle(String editDialogTitle) {
+		this.editDialogTitle = editDialogTitle;
+	}
+	
+	
 	public void setSelectedParties(List<DomibusConnectorParty> selectedParties) {
 		LOG.trace("#setSelectedParties: called with [{}]",  selectedParties);
 		this.selectedParties = selectedParties;
 	}
 	
+	
+	public String getEditDialogConfirmButtonText() {
+		return editDialogConfirmButtonText;
+	}
+
+	
+	public void setEditDialogConfirmButtonText(String editDialogConfirmButtonText) {
+		this.editDialogConfirmButtonText = editDialogConfirmButtonText;
+	}
+	
+	
+	/*
+	 * ACTION LISTENER, BUTTON HANDLER
+	 */
+	
+
+
+
 	public void deleteSelectedParties(ActionEvent actionEvent) {
 		LOG.trace("#deleteSelectedParties: delete [{}] following parties: [{}]", 
 				selectedParties == null ? 0 : selectedParties.size(), 
 						 selectedParties);			
 	}
 	
+	
 	public void confirmDeleteSelectedParties(ActionEvent actionEvent) {
 		LOG.trace("#confirmDeleteSelectedParties: delete confirmed, calling Service to delete [{}]", selectedParties);
 		//TODO: delete DB entries
 		
 		for (DomibusConnectorParty p : selectedParties) {			
-			this.pModeSupport.deleteParty(p);
+			//this.pModeSupport.deleteParty(p);
 			//TODO: handle exceptions in delete, decide abbort all deletions? -> should all deletions be in one transaction?
-		}
-		
-		
+		}			
 	}
-		
+	
+	
+	public void createNewParty(ActionEvent actionEvent) {
+		LOG.trace("#createNewParty: called");
+		this.party = new DomibusConnectorParty();
+		this.createNewPartyMode = true;
+	}
+	
+	
+	public void editParty() {
+		LOG.trace("#editParty: called with party: [{}]", this.party);
+		//TODO: handle edit...
+		this.createNewPartyMode = false;		
+	}
+
+	
+	public void saveParty(ActionEvent actionEvent) {
+		LOG.trace("#saveParty: called with party [{}] and mode createNewParty is [{}]", this.party, this.createNewPartyMode);
+		if (this.createNewPartyMode) {
+			//TODO: create new party
+		} else {
+			//TODO: save change...
+		}
+		this.party = null;		
+	}
+	
+	
 	
 
+
+	
+	
 }
