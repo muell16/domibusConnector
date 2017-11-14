21d5d1fa695c2b89a3f88c7f4b528118f26713fd
Stephan <stephan.spindler@activesolution.com>
2017-11-14 10:28:31 +0100

    Tomcat embedded is starting with JSF the context is not complete yet - managed beans are missing also login filter, ... spring-boot-maven-plugin added - deployment as war is not working yet

--- domibusWebAdmin/src/main/java/eu/domibus/webadmin/runner/Initializer.java
+++ domibusWebAdmin/src/main/java/eu/domibus/webadmin/runner/Initializer.java
@@ -5,18 +5,11 @@ import javax.servlet.ServletException;
 
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 
-public class Initializer implements ServletContextInitializer {
+import com.sun.faces.config.FacesInitializer;
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		
-		servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", "true");
-		servletContext.setInitParameter("primefaces.THEME", "bootstrap");
-		servletContext.setInitParameter("primefaces.FONT_AWESOME", "true");
-		servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", "true");
-		servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
-		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", "true");
-		
-	}
+
+public class Initializer  {
+
+
 
 }
--- domibusWebAdmin/src/main/java/eu/domibus/webadmin/runner/WebRunner.java
+++ domibusWebAdmin/src/main/java/eu/domibus/webadmin/runner/WebRunner.java
@@ -1,16 +1,27 @@
 package eu.domibus.webadmin.runner;
 
 import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
+import javax.faces.application.ApplicationFactory;
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletException;
 
+import org.primefaces.component.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +30,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.jsf.el.SpringBeanFacesELResolver;
+
+import com.sun.faces.config.ConfigureListener;
+import com.sun.faces.config.FacesInitializer;
 
 
 
@@ -26,25 +41,93 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-public class WebRunner  extends SpringBootServletInitializer  {
+public class WebRunner extends SpringBootServletInitializer implements ServletContextInitializer {
 
+	private final static Logger LOG = LoggerFactory.getLogger(WebRunner.class);
+	
 	public WebRunner() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
+		LOG.trace("main: run Spring application....");
 		SpringApplication.run(WebRunner.class, args);
 	}
 	
+	//TODO: add auth filter; file-upload-filter; ...
+	
+	
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
+    	LOG.trace("servletRegistrationBean: called");
         FacesServlet servlet = new FacesServlet();
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.xhtml", "*.jsf");
         servletRegistrationBean.setLoadOnStartup(1);
-        
+        servletRegistrationBean.addUrlMappings("/faces/*");
 		return servletRegistrationBean;
+		
+		
+		
+    }
+    
+//    @Bean
+//    public ServletListenerRegistrationBean<JsfApplicationObjectConfigureListener> jsfConfigureListener() {
+//        return new ServletListenerRegistrationBean<JsfApplicationObjectConfigureListener>(
+//                new JsfApplicationObjectConfigureListener());
+//    }
+
+    
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        servletContext.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
+        servletContext.setInitParameter("javax.faces.PARTIAL_STATE_SAVING_METHOD", "true");
+
+        servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+        servletContext.setInitParameter("facelets.DEVELOPMENT", "true");
+        servletContext.setInitParameter("javax.faces.FACELETS_REFRESH_PERIOD", "1");
+
+        servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", "true");
+		servletContext.setInitParameter("primefaces.THEME", "bootstrap");
+		servletContext.setInitParameter("primefaces.FONT_AWESOME", "true");
+		servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", "true");
+		servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+//		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", "true");
+		
+//		servletContext.setInitParameter("com.sun.faces.expressionFactory", "com.sun.el.ExpressionFactoryImpl");
+        
+        
+        Set<Class<?>> clazz = new HashSet<Class<?>>();
+
+        clazz.add(WebRunner.class); // dummy, enables InitFacesContext
+
+        FacesInitializer facesInitializer = new FacesInitializer();
+        facesInitializer.onStartup(clazz, servletContext);
+
+    }
+    
+
+    @Bean
+    public ServletListenerRegistrationBean<JsfApplicationObjectConfigureListener> jsfConfigureListener() {
+    	LOG.trace("jsfConfigureListener: called");
+        return new ServletListenerRegistrationBean<JsfApplicationObjectConfigureListener>(
+                new JsfApplicationObjectConfigureListener());
     }
 
+
+    static class JsfApplicationObjectConfigureListener extends ConfigureListener {
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            super.contextInitialized(sce);
+
+            ApplicationFactory factory = (ApplicationFactory) FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
+            Application app = factory.getApplication();
+
+            app.addELResolver(new SpringBeanFacesELResolver());
+        }
+    }
+    
 //    @Bean
 //    public FilterRegistrationBean rewriteFilter() {
 //        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
