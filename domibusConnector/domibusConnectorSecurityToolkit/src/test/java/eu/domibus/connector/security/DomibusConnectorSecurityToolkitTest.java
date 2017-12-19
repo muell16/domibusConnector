56d161b2c2e039f10d45eb8953fb1344da218bac
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2017-12-19 09:55:54 +0100

    tests extended

--- domibusConnector/domibusConnectorSecurityToolkit/src/test/java/eu/domibus/connector/security/DomibusConnectorSecurityToolkitTest.java
+++ domibusConnector/domibusConnectorSecurityToolkit/src/test/java/eu/domibus/connector/security/DomibusConnectorSecurityToolkitTest.java
@@ -1,25 +1,29 @@
 package eu.domibus.connector.security;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import javax.annotation.Resource;
-
+import eu.domibus.connector.common.message.Message;
+import eu.domibus.connector.common.message.MessageAttachment;
+import eu.domibus.connector.common.message.MessageContent;
+import eu.domibus.connector.common.message.MessageDetails;
+import eu.domibus.connector.security.container.DomibusSecurityContainer;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Assert;
+import org.xmlunit.diff.DefaultNodeMatcher;
+import org.xmlunit.diff.ElementSelectors;
+import org.xmlunit.matchers.CompareMatcher;
 
-import eu.domibus.connector.common.message.Message;
-import eu.domibus.connector.common.message.MessageAttachment;
-import eu.domibus.connector.common.message.MessageContent;
-import eu.domibus.connector.common.message.MessageDetails;
-import eu.domibus.connector.security.container.DomibusSecurityContainer;
+import static org.assertj.core.api.Assertions.*;
+import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/context/DomibusConnectorSecurityToolkitContext.xml",
@@ -27,6 +31,8 @@ import eu.domibus.connector.security.container.DomibusSecurityContainer;
 public class DomibusConnectorSecurityToolkitTest {
 
 	static Logger LOGGER = LoggerFactory.getLogger(DomibusConnectorSecurityToolkitTest.class);
+
+	public static String TEST_FILE_RESULTS_DIR_PROPERTY_NAME = "test.file.results";
 
 	@Resource
 	private DomibusSecurityContainer securityContainer;
@@ -63,7 +69,8 @@ public class DomibusConnectorSecurityToolkitTest {
 			e.printStackTrace();
 		}
 
-		Assert.notEmpty(message.getAttachments());
+		assertThat(message.getAttachments()).isNotEmpty();
+		//Assert.notEmpty(message.getAttachments());
 
 		securityContainer.recieveContainerContents(message);
 
@@ -71,12 +78,31 @@ public class DomibusConnectorSecurityToolkitTest {
 
 			if (attachment.getName().equals("Token.xml")) {
 				writeResult(resultName +".xml", attachment.getAttachment());
+				//test xml
+                if ("signedResultToken".equals(resultName)) {
+//TODO: compare resulting xml!
+//                    Assert.assertThat(attachment.getAttachment(),
+//                            isSimilarTo("control xml").withNodeMatcher(
+//                                    new DefaultNodeMatcher(ElementSelectors.byName)));
+                } else if ("unsignedResultToken".equals(resultName)) {
+
+                } else {
+                    throw new IllegalStateException("should not end up here! Passed unsupported result name!");
+                }
+
+
+
 			} else if (attachment.getName().equals("Token.pdf")) {
 				writeResult(resultName +".pdf", attachment.getAttachment());
 			}
 		}
 	}
 
+	private static String getTestFilesDir() {
+		String dir = System.getProperty(TEST_FILE_RESULTS_DIR_PROPERTY_NAME, "." + File.separator + "target" + File.separator + "testfileresults" + File.separator);
+		dir = dir + DomibusConnectorSecurityToolkitTest.class.getSimpleName();
+		return dir;
+	}
 
 	private static String getRessourceFolder(){
 		String dir = System.getProperty("user.dir");
@@ -86,8 +112,9 @@ public class DomibusConnectorSecurityToolkitTest {
 
 	}
 
-	private static byte[] readRessource(final String name){
-		try{
+	private static byte[] readRessource(final String name) {
+		try {
+
 			File file = new File(getRessourceFolder() + "examples/" + name);
 
 			FileInputStream fileInputStream = new FileInputStream(file);
@@ -97,18 +124,20 @@ public class DomibusConnectorSecurityToolkitTest {
 
 			return data;
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
 	private static void writeResult(final String name, final byte[] data){
+		LOGGER.info("writeResult: writing to [{}]", name);
 		try {
-			FileOutputStream fos = new FileOutputStream(getRessourceFolder() + "result/" + name);
+			File dir = new File(getTestFilesDir() + File.separator );
+			dir.mkdirs();
+			FileOutputStream fos = new FileOutputStream(dir + File.separator + name);
 			fos.write(data);
 			fos.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 }
