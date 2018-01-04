3337c520957c6f6c7477bcb59530d57cc7d71389
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-01-04 17:21:09 +0100

    marked GlobalConstants as deprecated

--- domibusConnector/domibusConnectorCommon/src/main/java/eu/domibus/connector/common/CommonConnectorGlobalConstants.java
+++ domibusConnector/domibusConnectorCommon/src/main/java/eu/domibus/connector/common/CommonConnectorGlobalConstants.java
@@ -1,15 +1,21 @@
 package eu.domibus.connector.common;
 
+@Deprecated
 public class CommonConnectorGlobalConstants {
 
+    @Deprecated
     public static final String MAIN_DOCUMENT_NAME = "mainDocument";
     
+    @Deprecated
     public static final String ASICS_CONTAINER_IDENTIFIER = "ASIC-S";
+    @Deprecated
     public static final String TOKEN_XML_IDENTIFIER = "tokenXML";
+    @Deprecated
     public static final String TOKEN_PDF_IDENTIFIER = "tokenPDF";
-    
+    @Deprecated
     public static final String CONTENT_PDF_IDENTIFIER = "ContentPDF";
+    @Deprecated
     public static final String CONTENT_XML_IDENTIFIER = "ContentXML";
-    
+    @Deprecated
     public static final String OLD_CONTENT_XML_NAME = "ECodexContentXML";
 }
