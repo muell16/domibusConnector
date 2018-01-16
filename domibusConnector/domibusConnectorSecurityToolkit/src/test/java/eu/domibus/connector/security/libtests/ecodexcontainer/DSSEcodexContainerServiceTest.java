aebe42b68196d4b969cbf0c39a325b411f318795
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-01-16 15:39:18 +0100

    extended security toolkit exploration testsextended security toolkit

--- domibusConnector/domibusConnectorSecurityToolkit/src/test/java/eu/domibus/connector/security/libtests/ecodexcontainer/DSSEcodexContainerServiceTest.java
+++ domibusConnector/domibusConnectorSecurityToolkit/src/test/java/eu/domibus/connector/security/libtests/ecodexcontainer/DSSEcodexContainerServiceTest.java
@@ -3,9 +3,12 @@ package eu.domibus.connector.security.libtests.ecodexcontainer;
 import eu.domibus.connector.security.container.DomibusSecurityContainer;
 import eu.ecodex.dss.model.BusinessContent;
 import eu.ecodex.dss.model.CertificateStoreInfo;
+import eu.ecodex.dss.model.ECodexContainer;
+import eu.ecodex.dss.model.EnvironmentConfiguration;
+import eu.ecodex.dss.model.ProxyData;
 import eu.ecodex.dss.model.SignatureParameters;
+import eu.ecodex.dss.model.token.AdvancedSystemType;
 import eu.ecodex.dss.model.token.TokenIssuer;
-import eu.ecodex.dss.service.ECodexException;
 import eu.ecodex.dss.service.ECodexLegalValidationService;
 import eu.ecodex.dss.service.impl.dss.DSSECodexContainerService;
 import eu.ecodex.dss.service.impl.dss.DSSECodexLegalValidationService;
@@ -16,12 +19,14 @@ import eu.europa.esig.dss.DigestAlgorithm;
 import eu.europa.esig.dss.EncryptionAlgorithm;
 import eu.europa.esig.dss.InMemoryDocument;
 import eu.europa.esig.dss.MimeType;
-import eu.europa.esig.dss.validation.CertificateVerifier;
+import eu.europa.esig.dss.client.http.proxy.ProxyConfig;
+import eu.europa.esig.dss.client.http.proxy.ProxyProperties;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 import static org.assertj.core.api.Assertions.*;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.util.StreamUtils;
 
 /**
@@ -29,15 +34,33 @@ import org.springframework.util.StreamUtils;
  * @author {@literal Stephan Spindler <stephan.spindler@extern.brz.gv.at> }
  */
 public class DSSEcodexContainerServiceTest {
+
+    private Properties properties;
+    
+    @Before
+    public void setUp() {
+        this.properties = loadTestProperties();
+    }
     
     
     private DSSECodexContainerService initContainerService() throws Exception {
         DSSECodexContainerService containerService = new DSSECodexContainerService();
         
+        
+        EnvironmentConfiguration environmentConfiguration = initEnvironmentConfiguration();
+        
+        
         ECodexLegalValidationService ecodexLegalValidationService = new DSSECodexLegalValidationService();
+        ecodexLegalValidationService.setEnvironmentConfiguration(environmentConfiguration);             
         containerService.setLegalValidationService(ecodexLegalValidationService);
         
+        
         DSSECodexTechnicalValidationService technicalValidationService = new DSSECodexTechnicalValidationService();
+        technicalValidationService.setEnvironmentConfiguration(environmentConfiguration);
+        technicalValidationService.setProxyPreferenceManager(initProxyConfig());
+        
+        technicalValidationService.initAuthenticationCertificateVerification();
+        
         containerService.setTechnicalValidationService(technicalValidationService);
         
 
@@ -56,26 +79,100 @@ public class DSSEcodexContainerServiceTest {
         
 //        CertificateVerifier certificateVerifier = Mockito.mock(CertificateVerifier.class);        
 //        containerService.setCertificateVerifier(certificateVerifier);
-        
-        
+                
         return containerService;
+    }
+    
+    private EnvironmentConfiguration initEnvironmentConfiguration() {
+        EnvironmentConfiguration envConfig = new EnvironmentConfiguration();         
+        envConfig.setProxyHTTPS(initHttpsProxyData());                
+        envConfig.setProxyHTTP(initHttpProxyData());
+        return envConfig;
+    }
+    
+    //ecodex container proxy config
+    private ProxyConfig initProxyConfig() {
+        ProxyConfig proxyConfig = new ProxyConfig();
+        
+        ProxyProperties httpsProxyProperties = new ProxyProperties();
+        httpsProxyProperties.setHost(properties.getProperty("https.proxy.host"));
+        httpsProxyProperties.setPort(Integer.valueOf(properties.getProperty("https.proxy.port")));
+        httpsProxyProperties.setPassword(properties.getProperty("https.proxy.password"));
+        httpsProxyProperties.setUser(properties.getProperty("https.proxy.user"));
+        proxyConfig.setHttpsProperties(httpsProxyProperties);
+        
+        ProxyProperties httpProxyProperties = new ProxyProperties();
+        httpProxyProperties.setHost(properties.getProperty("http.proxy.host"));
+        httpProxyProperties.setPort(Integer.valueOf(properties.getProperty("http.proxy.port")));
+        httpProxyProperties.setPassword(properties.getProperty("http.proxy.password"));
+        httpProxyProperties.setUser(properties.getProperty("http.proxy.user"));
+        proxyConfig.setHttpProperties(httpProxyProperties);
+        
+        return proxyConfig;
+    }
+    
+    private ProxyData initHttpsProxyData() {
+        ProxyData httpProxy = new ProxyData();
+        httpProxy.setPort(Integer.valueOf(properties.getProperty("https.proxy.port")));
+        httpProxy.setHost(properties.getProperty("https.proxy.host"));
+        httpProxy.setAuthName(properties.getProperty("https.proxy.user"));
+        httpProxy.setAuthPass(properties.getProperty("https.proxy.password"));
+        return httpProxy;
+    }
+        
+    private ProxyData initHttpProxyData() {
+        ProxyData httpProxy = new ProxyData();
+        httpProxy.setPort(Integer.valueOf(properties.getProperty("http.proxy.port")));
+        httpProxy.setHost(properties.getProperty("http.proxy.host"));
+        httpProxy.setAuthName(properties.getProperty("http.proxy.user"));
+        httpProxy.setAuthPass(properties.getProperty("http.proxy.password"));
+        return httpProxy;
+    }
+    
+    private Properties loadTestProperties() {
+        try {
+            Properties loadedTestProperties = new Properties();
+            InputStream testPropertiesInputStream = this.getClass().getResourceAsStream("/test.properties");
+            if (testPropertiesInputStream == null) {
+                throw new RuntimeException("test.properties could not be load as resource!");
+            }        
+            loadedTestProperties.load(testPropertiesInputStream);
+            return loadedTestProperties;
+        } catch (IOException ioe) {
+            throw new RuntimeException("test.properties could not be read!");
+        }
+    }
+    
+    
+    @Test
+    public void testCreateBigContainer() throws Exception {
+        
     }
     
     
     @Test
     public void testCreateContainer() throws Exception {
         DSSECodexContainerService containerService = initContainerService(); 
-        
-        
-        
+                        
         BusinessContent businessContent = new BusinessContent();
         
-        DSSDocument mainDocument = new InMemoryDocument(
-                            loadByteArrayFromClassPathRessource("/examples/ExmaplePdf.pdf"),
+        DSSDocument xmlDocument = new InMemoryDocument(
+            loadByteArrayFromClassPathRessource("/examples/Form_A.xml"),
+            DomibusSecurityContainer.CONTENT_XML_IDENTIFIER + ".xml", 
+            MimeType.PDF);        
+   
+        businessContent.setDocument(xmlDocument);
+        
+        
+        DSSDocument formAPdf = new InMemoryDocument(
+                            loadByteArrayFromClassPathRessource("/examples/Form_A.pdf"),
                             DomibusSecurityContainer.MAIN_DOCUMENT_NAME + ".pdf", 
                             MimeType.PDF);        
-        businessContent.setDocument(mainDocument);
+        businessContent.addAttachment(formAPdf);
         
+        //big dss document (4GB random bytes)
+//        DSSDocument doc = new MyBigDssFile();
+//        businessContent.addAttachment(doc);
         
         DSSDocument attachmentOne = new InMemoryDocument(
                 loadByteArrayFromClassPathRessource("/examples/supercool.pdf"),
@@ -84,14 +181,21 @@ public class DSSEcodexContainerServiceTest {
         
         businessContent.addAttachment(attachmentOne);
         
-
-        TokenIssuer tokenIssuer = new TokenIssuer();
+        TokenIssuer tokenIssuer = createTokenIssuer();
         
-        containerService.create(businessContent, tokenIssuer);
+        ECodexContainer container = containerService.create(businessContent, tokenIssuer);
+        DSSDocument asicDocument = container.getAsicDocument();
+        assertThat(asicDocument).isNotNull();
         
     }
     
-    
+    private TokenIssuer createTokenIssuer() {
+        TokenIssuer tokenIssuer = new TokenIssuer();
+        tokenIssuer.setAdvancedElectronicSystem(AdvancedSystemType.SIGNATURE_BASED);
+        tokenIssuer.setCountry("AT");
+        tokenIssuer.setServiceProvider("BRZ");
+        return tokenIssuer;
+    }
     
     private byte[] loadByteArrayFromClassPathRessource(String ressource) {
         try {
