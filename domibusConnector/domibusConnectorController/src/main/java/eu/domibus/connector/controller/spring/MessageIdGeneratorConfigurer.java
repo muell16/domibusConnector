033db902a5afcf974c838f6fe91fa5ceba4565e4
riederb <bernhard.rieder@extern.brz.gv.at>
2018-01-18 08:23:49 +0100

    moved and adopted message generator

--- domibusConnector/domibusConnectorController/src/main/java/eu/domibus/connector/controller/spring/MessageIdGeneratorConfigurer.java
+++ domibusConnector/domibusConnectorController/src/main/java/eu/domibus/connector/controller/spring/MessageIdGeneratorConfigurer.java
@@ -1,11 +1,12 @@
 package eu.domibus.connector.controller.spring;
 
-import eu.domibus.connector.controller.service.DomibusConnectorMessageIdGenerator;
-import java.util.Date;
 import java.util.UUID;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import eu.domibus.connector.controller.service.DomibusConnectorMessageIdGenerator;
 
 /**
  *
@@ -19,7 +20,7 @@ public class MessageIdGeneratorConfigurer {
     @ConditionalOnMissingBean(DomibusConnectorMessageIdGenerator.class)
     @Bean
     public DomibusConnectorMessageIdGenerator domibusConnectorMessageIdGenerator() {
-        return () -> String.format("%s-%s", UUID.randomUUID(), new Date());
+        return () -> String.format("%s@%s", UUID.randomUUID(), "domibus.connector.eu");
     }
     
     
