c927bed6f2d0b8a72e608aa12c277602e1a75792
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-02-08 16:44:42 +0100

    fix build bug

--- domibusConnector/domibusConnectorBackendLink/src/main/java/eu/domibus/connector/ws/backend/link/impl/PushMessageViaWsToBackendClientImpl.java
+++ domibusConnector/domibusConnectorBackendLink/src/main/java/eu/domibus/connector/ws/backend/link/impl/PushMessageViaWsToBackendClientImpl.java
@@ -1,7 +1,6 @@
 
 package eu.domibus.connector.ws.backend.link.impl;
 
-import com.sun.xml.internal.ws.policy.Policy;
 import eu.domibus.connector.backend.domain.model.DomibusConnectorBackendClientInfo;
 import eu.domibus.connector.backend.persistence.service.BackendClientInfoPersistenceService;
 import eu.domibus.connector.domain.model.DomibusConnectorMessage;
@@ -10,41 +9,13 @@ import eu.domibus.connector.domain.transition.DomibsConnectorAcknowledgementType
 import eu.domibus.connector.domain.transition.DomibusConnectorMessageType;
 import eu.domibus.connector.persistence.service.DomibusConnectorMessagePersistenceService;
 import eu.domibus.connector.persistence.service.DomibusConnectorPersistAllBigDataOfMessageService;
-import eu.domibus.connector.ws.backend.delivery.webservice.DomibusConnectorBackendDeliveryWSService;
 import eu.domibus.connector.ws.backend.delivery.webservice.DomibusConnectorBackendDeliveryWebService;
-import eu.domibus.connector.ws.backend.webservice.DomibusConnectorBackendWebService;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Properties;
-import java.util.logging.Level;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.ws.WebServiceFeature;
-import javax.xml.ws.soap.MTOMFeature;
-import org.apache.cxf.Bus;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.feature.Feature;
-import org.apache.cxf.frontend.ClientFactoryBean;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.staxutils.StaxUtils;
-import org.apache.cxf.ws.policy.PolicyBuilderImpl;
-import org.apache.cxf.ws.policy.PolicyEngine;
-import org.apache.cxf.ws.policy.WSPolicyFeature;
-import org.apache.neethi.PolicyReference;
 //import org.apache.cxf.ws.policy.v200607.PolicyReference;
 //import org.apache.neethi.Policy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.Element;
 
 /**
  *
