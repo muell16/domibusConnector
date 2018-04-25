1bb4a4d1cd97865b9077672bf7416afd87052a66
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-04-25 17:46:51 +0200

    bugfix for null trust store

--- domibusConnector/domibusConnectorSecurityToolkit/src/main/java/eu/domibus/connector/security/configuration/DomibusConnectorEnvironmentConfiguration.java
+++ domibusConnector/domibusConnectorSecurityToolkit/src/main/java/eu/domibus/connector/security/configuration/DomibusConnectorEnvironmentConfiguration.java
@@ -1,6 +1,9 @@
 package eu.domibus.connector.security.configuration;
 
+import eu.domibus.connector.lib.spring.configuration.StoreConfigurationProperties;
 import eu.domibus.connector.security.spring.SecurityToolkitConfigurationProperties;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,21 +15,27 @@ import eu.ecodex.dss.model.EnvironmentConfiguration;
 @Component("domibusConnectorEnvironmentConfiguration")
 public class DomibusConnectorEnvironmentConfiguration extends EnvironmentConfiguration implements InitializingBean {
 
-	@Autowired
-	SecurityToolkitConfigurationProperties securityToolkitConfigurationProperties;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DomibusConnectorEnvironmentConfiguration.class);
 
-//	@Value("${connector.truststore.path:null}")
-//	String truststoreLocation;
-//	@Value("${connector.truststore.password:null}")
-//	String truststorePassword;
+	private SecurityToolkitConfigurationProperties securityToolkitConfigurationProperties;
+
+	@Autowired
+	public void setSecurityToolkitConfigurationProperties(SecurityToolkitConfigurationProperties securityToolkitConfigurationProperties) {
+		this.securityToolkitConfigurationProperties = securityToolkitConfigurationProperties;
+	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		CertificateStoreInfo trustedCertificates = new CertificateStoreInfo();
-		trustedCertificates.setLocation(securityToolkitConfigurationProperties.getTrustStore().getPath().getURL().toString());
-		trustedCertificates.setPassword(securityToolkitConfigurationProperties.getTrustStore().getPassword());
-		
-		setConnectorCertificates(trustedCertificates);
+        StoreConfigurationProperties trustStore = securityToolkitConfigurationProperties.getTrustStore();
+        if (trustStore != null && trustStore.getPath() != null) {
+            trustedCertificates.setLocation(trustStore.getPath().getURL().toString());
+            trustedCertificates.setPassword(trustStore.getPassword());
+            setConnectorCertificates(trustedCertificates);
+            LOGGER.info("Setting trusted certificates trustStore to: [{}]", trustStore.getPath().getURL().toString());
+        } else {
+            LOGGER.info("Not setting trusted certificates, because no trusted certificates trust store is configured!");
+        }
 	}
 
 }
---
+++ domibusConnector/domibusConnectorSecurityToolkit/src/test/java/eu/domibus/connector/security/configuration/DomibusConnectorEnvironmentConfigurationTest.java
@@ -0,0 +1,48 @@
+package eu.domibus.connector.security.configuration;
+
+
+import eu.domibus.connector.lib.spring.configuration.StoreConfigurationProperties;
+import eu.domibus.connector.security.spring.SecurityToolkitConfigurationProperties;
+import eu.ecodex.dss.model.EnvironmentConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+public class DomibusConnectorEnvironmentConfigurationTest {
+
+
+    /**
+     * The truststore can be null, if this is the case
+     * the configuration should not check the asics certs
+     *
+     */
+    @Test
+    public void testEnvironmentConfigurationWithNullTrustStore() throws Exception {
+        SecurityToolkitConfigurationProperties props = new SecurityToolkitConfigurationProperties();
+        DomibusConnectorEnvironmentConfiguration envConf = new DomibusConnectorEnvironmentConfiguration();
+        envConf.setSecurityToolkitConfigurationProperties(props);
+        envConf.afterPropertiesSet();
+    }
+
+
+    @Test
+    public void testEnvironmentConfigurationWithTrustStore() throws Exception {
+        DomibusConnectorEnvironmentConfiguration envConf = new DomibusConnectorEnvironmentConfiguration();
+        envConf.setSecurityToolkitConfigurationProperties(generateSecurityToolkitConfigurationProperties());
+        envConf.afterPropertiesSet();
+
+    }
+
+
+    private SecurityToolkitConfigurationProperties generateSecurityToolkitConfigurationProperties() {
+        SecurityToolkitConfigurationProperties props = new SecurityToolkitConfigurationProperties();
+
+        StoreConfigurationProperties trustStore = new StoreConfigurationProperties();
+        trustStore.setPath(new ClassPathResource("/keys/ojStore.jks"));
+        trustStore.setPassword("ecodex");
+        props.setTrustStore(trustStore);
+
+        return props;
+    }
+
+}
