ae5464b4312e12004a3dcef0e0049d8fdf8f6786
RiederB <bernhard.rieder@brz.gv.at>
2018-07-20 14:18:24 +0200

    vaading gui with connector startable as spring boot

--- domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/MainView.java
+++ domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/MainView.java
@@ -6,78 +6,40 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.annotation.UIScope;
 
 import eu.domibus.connector.web.viewAreas.Messages;
 
 @HtmlImport("styles/shared-styles.html")
-@Route("domibusConnector/")
-@org.springframework.stereotype.Component
-@UIScope
+@Route
 public class MainView extends VerticalLayout {
-
+	
 	Div areaMessages = null;
 	Tab messagesTab = new Tab("Messages");
 	Tabs TopMenu = new Tabs();
-	
-	public MainView() {
-		
-		
-		areaMessages = new Div();
-		areaMessages.add(new Messages());
+
+    public MainView(@Autowired ExampleTemplate template, @Autowired Messages messages) {
+//        // This is just a simple label created via Elements API
+//        Button button = new Button("Click me",
+//                event -> template.setValue("Clicked!"));
+//        // This is a simple template example
+//        add(button, template);
+//        setClassName("main-layout");
+        
+        areaMessages = new Div();
+		areaMessages.add(messages);
 		areaMessages.setVisible(true);
-		
-		
-		
-//		detailsERV = new Div();
-//		//detailsERV.setText("Details not implemented Yet");
-//		ervDetails = new ERVDetailsComponent(conf);
-//		detailsERV.add(ervDetails);
-//		detailsERV.setWidth("100vh");
-//		detailsERV.setVisible(false);
-		
-		
-//		logDetails = new Div();
-//		
-//		logDetails.add(logger);
-//		logDetails.setVisible(false);
-//		
-//
-//		
-//		
-//		demoDiv = new Div();
-//		demoDiv.add(new  MainViewComponent());
-//		demoDiv.setVisible(false);
-		
-		
-//		close.addClickListener(e -> { 
-//			close.getUI().ifPresent(ui -> ui.navigate("logout/"));
-//			});
-//		
-//		
-//		Close.add(close);
-		
-		
-//		exitDiv = new Div();
-//		exitDiv.add(new LogoutComponent());
-//		exitDiv.setVisible(false);
-		
 		
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		tabsToPages.put(messagesTab, areaMessages);
-//		tabsToPages.put(ErvOverviewTab, areaERV);
-//		tabsToPages.put(ErvDetails, detailsERV);
-//		tabsToPages.put(LogTab, logDetails);
-//		tabsToPages.put(Demo, demoDiv);
-//		tabsToPages.put(Close, exitDiv);
 		
 		
 		TopMenu.add(messagesTab);
@@ -96,12 +58,6 @@ public class MainView extends VerticalLayout {
 		});
 
 		add(TopMenu,pages);
-	}
-//	
-//	public void showERVMessageDetails(String ervMsgID) {
-//		ervDetails.loadErvDetails(ervMsgID);
-//		ervDetails.setAlignItems(Alignment.START);
-//		TopMenu.setSelectedTab(ErvDetails);
-//	}
+    }
 
 }
