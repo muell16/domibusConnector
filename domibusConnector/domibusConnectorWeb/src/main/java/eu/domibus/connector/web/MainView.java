ba12d32a8c13da3f7765274d7261aadcec05fda8
RiederB <bernhard.rieder@brz.gv.at>
2018-07-27 09:49:15 +0200

    created header for admin

--- domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/MainView.java
+++ domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/MainView.java
@@ -11,7 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
@@ -25,28 +29,30 @@ import eu.domibus.connector.web.viewAreas.pmodes.PModes;
 @PageTitle("domibusConnector")
 public class MainView extends VerticalLayout {
 	
-	Div areaMessages = null;
-	Div areaPModes = null;
-	
-	Tab messagesTab = new Tab("Messages");
-	Tab pmodesTab = new Tab("PModes");
-	
-	Tabs TopMenu = new Tabs();
 
     public MainView(@Autowired Messages messages, @Autowired PModes pmodes) {
         
-        areaMessages = new Div();
+    	HorizontalLayout header = createHeader();
+    	
+    	
+    	Div areaMessages = new Div();
 		areaMessages.add(messages);
 		areaMessages.setVisible(true);
 		
-		areaPModes = new Div();
+		Div areaPModes = new Div();
 		areaPModes.add(pmodes);
 		areaPModes.setVisible(false);
+		
+		Tab messagesTab = new Tab("Messages");
+		messagesTab.getStyle().set("font-size", "20px");
+		Tab pmodesTab = new Tab("PModes");
+		pmodesTab.getStyle().set("font-size", "20px");
 		
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		tabsToPages.put(messagesTab, areaMessages);
 		tabsToPages.put(pmodesTab, areaPModes);
 		
+		Tabs TopMenu = new Tabs();
 		
 		TopMenu.add(messagesTab);
 		TopMenu.add(pmodesTab);
@@ -65,7 +71,44 @@ public class MainView extends VerticalLayout {
 		    pagesShown.add(selectedPage);
 		});
 
-		add(TopMenu,pages);
+		add(header,TopMenu,pages);
     }
+
+	private HorizontalLayout createHeader() {
+		
+		Div ecodexLogo = new Div();
+		Image ecodex = new Image("frontend/images/logo_ecodex_0.png", "eCodex");
+		ecodex.setHeight("70px");
+		ecodexLogo.add(ecodex);
+		ecodexLogo.setHeight("70px");
+		
+		
+		Div domibusConnector = new Div();
+		Label dC = new Label("domibusConnector - Administration");
+		dC.getStyle().set("font-size", "30px");
+		dC.getStyle().set("font-style", "italic");
+		dC.getStyle().set("color", "grey");
+		domibusConnector.add(dC);
+		domibusConnector.getStyle().set("text-align", "center");
+		
+		
+		Div europaLogo = new Div();
+		Image europa = new Image("frontend/images/europa-logo.jpg", "europe");
+		europa.setHeight("50px");
+		europaLogo.add(europa);
+		europaLogo.setHeight("50px");
+		europaLogo.getStyle().set("margin-right", "3em");
+		
+		
+		HorizontalLayout headerLayout = new HorizontalLayout(ecodexLogo, domibusConnector, europaLogo);
+		headerLayout.setAlignItems(Alignment.CENTER);
+		headerLayout.expand(domibusConnector);
+		headerLayout.setJustifyContentMode(com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode.CENTER);
+		headerLayout.setWidth("100vw");
+		headerLayout.getStyle().set("border-bottom", "1px solid #9E9E9E");
+		headerLayout.getStyle().set("padding-bottom", "16px");
+		
+		return headerLayout;
+	}
 
 }
