ef38435588e0645fa53687aa2ee19f130db66a79
Stephan <spind42@gmail.com>
2018-11-20 23:08:49 +0100

    update docker build

--- domibusConnector/domibusConnectorLauncher/domibusConnectorStandaloneLauncher/src/main/bash/start.sh
+++ domibusConnector/domibusConnectorLauncher/domibusConnectorStandaloneLauncher/src/main/bash/start.sh
@@ -4,6 +4,6 @@
 #CLASSPATH=$WORKDIR/lib/*
 #java -Dspring.config.location=$WORKDIR/config -Dspring.config.name=connector -Dspring.cloud.bootstrap.location=$WORKDIR/config/bootstrap.properties -cp $CLASSPATH org.springframework.boot.loader.PropertiesLauncher
 
-java -Dloader.path=./lib -Dspring.config.name=connector -cp ./lib/connector.jar org.springframework.boot.loader.PropertiesLauncher
+java -Dloader.path=./lib -Dspring.config.name=connector -cp ./bin/connector.jar org.springframework.boot.loader.PropertiesLauncher
 
 
