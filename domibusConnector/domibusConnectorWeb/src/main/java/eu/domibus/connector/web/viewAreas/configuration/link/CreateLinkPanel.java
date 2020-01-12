5c9d38d179029e80b74a5bed63425218a201c0c5
Stephan <spind42@gmail.com>
2020-01-12 23:22:26 +0100

    further work on UI config of plugin

--- domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/viewAreas/configuration/link/CreateLinkPanel.java
+++ domibusConnector/domibusConnectorWeb/src/main/java/eu/domibus/connector/web/viewAreas/configuration/link/CreateLinkPanel.java
@@ -1,19 +1,33 @@
 package eu.domibus.connector.web.viewAreas.configuration.link;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.ItemLabelGenerator;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.data.renderer.Renderer;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 import eu.domibus.connector.domain.model.DomibusConnectorLinkConfiguration;
+import eu.domibus.connector.link.api.LinkPlugin;
+import eu.domibus.connector.link.service.DCActiveLinkManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+@Scope(SCOPE_PROTOTYPE)
+@org.springframework.stereotype.Component
 public class CreateLinkPanel extends VerticalLayout {
 
     private ProgressBar progressBar = new ProgressBar(10, 100, 10);
@@ -22,26 +36,59 @@ public class CreateLinkPanel extends VerticalLayout {
     private Text header = new Text ("Create Link");
     private Div content = new Div();
 
+    @Autowired
+    DCActiveLinkManagerService linkManagerService;
 
     List<Component> steps = new ArrayList<>();
 
     private DomibusConnectorLinkConfiguration domibusConnectorLinkConfiguration;
 
 
-    public CreateLinkPanel() {
+    @PostConstruct
+    private void init() {
         initUI();
         initSteps();
     }
 
     private void initUI() {
 
+        initSteps();
+
         add(header);
         add(progressBar);
         add(content);
         add(new HorizontalLayout(backButton, forwardButton));
+
+        this.content.add(this.steps.get(0));
+
     }
 
     private void initSteps() {
+        this.steps.add(new ChooseImplStep());
+    }
+
+    public class ChooseImplStep extends VerticalLayout {
+        private ComboBox<LinkPlugin> implChooser = new ComboBox<>();
+
+        public ChooseImplStep() {
+            initUI();
+        }
+
+        private void initUI() {
+
+            implChooser.setItems(linkManagerService.getAvailableLinkPlugins());
+            implChooser.setItemLabelGenerator((ItemLabelGenerator<LinkPlugin>) LinkPlugin::getPluginName);
+            add(implChooser);
+            implChooser.addValueChangeListener(this::valueChanged);
+
+        }
+
+
+        private void valueChanged(HasValue.ValueChangeEvent<LinkPlugin> valueChangeEvent) {
+            LinkPlugin value = valueChangeEvent.getValue();
+//            value.getPluginConfigurationProperties()
+        }
+
 
     }
 
