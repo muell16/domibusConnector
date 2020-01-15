9f1c9b69e2cdfd121e828a91c6e3772c06d591ef
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2020-01-15 09:07:42 +0100

    update code

--- domibusConnector/domibusConnectorLinkCommon/src/main/java/eu/domibus/connector/link/service/DCPluginBasedBackendDeliveryService.java
+++ domibusConnector/domibusConnectorLinkCommon/src/main/java/eu/domibus/connector/link/service/DCPluginBasedBackendDeliveryService.java
@@ -3,9 +3,11 @@ package eu.domibus.connector.link.service;
 import eu.domibus.connector.controller.exception.DomibusConnectorControllerException;
 import eu.domibus.connector.controller.service.DomibusConnectorBackendDeliveryService;
 import eu.domibus.connector.domain.model.DomibusConnectorMessage;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("linkplugins")
 public class DCPluginBasedBackendDeliveryService implements DomibusConnectorBackendDeliveryService {
 
     @Override
