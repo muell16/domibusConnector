e6023b0f180bf08fb65755f5d6558a2ce9798e5e
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2020-02-11 17:25:54 +0100

    update doc

---
+++ domibusConnector/domibusConnectorDocumentation/src/main/resources/DbScripts/migration/Oracle_Migrate_4.1.0_ConnectorDB_to_4.1.8.sql
@@ -0,0 +1,7 @@
+-- *********************************************************************
+-- Update Database Script - from domibusConnector 4.1 to 4.1.8
+-- *********************************************************************
+--
+--
+--
+ALTER TABLE DOMIBUS_CONNECTOR_PROPERTY ADD COLUMN ID int NOT NULL;
