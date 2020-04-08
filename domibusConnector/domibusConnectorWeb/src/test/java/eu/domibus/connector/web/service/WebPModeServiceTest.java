f88ee5f9d00face4dd7630fc2e7376d34c0caf47
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2020-04-08 09:43:00 +0200

    fix mistake

--- domibusConnector/domibusConnectorWeb/src/test/java/eu/domibus/connector/web/service/WebPModeServiceTest.java
+++ domibusConnector/domibusConnectorWeb/src/test/java/eu/domibus/connector/web/service/WebPModeServiceTest.java
@@ -34,8 +34,7 @@ public class WebPModeServiceTest {
             return new WebPModeService();
         }
     }
-
-    LiquibaseUpgradeInitITCase
+    
     @Autowired
     WebPModeService webPModeService;
 
