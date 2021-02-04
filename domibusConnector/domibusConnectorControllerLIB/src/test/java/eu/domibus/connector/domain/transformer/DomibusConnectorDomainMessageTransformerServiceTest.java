2200067a48dbe20191f2d9a45373c2f23d5f63c2
Stephan Spindler <stephan.spindler@brz.gv.at>
2021-02-04 10:56:19 +0100

    add fix for empty document

--- domibusConnector/domibusConnectorControllerLIB/src/test/java/eu/domibus/connector/domain/transformer/DomibusConnectorDomainMessageTransformerServiceTest.java
+++ domibusConnector/domibusConnectorControllerLIB/src/test/java/eu/domibus/connector/domain/transformer/DomibusConnectorDomainMessageTransformerServiceTest.java
@@ -345,6 +345,20 @@ public class DomibusConnectorDomainMessageTransformerServiceTest {
     }
 
     @Test
+    public void testTransformTransitionToDomain_NoBusinessDoc() {
+        DomibusConnectorMessageType message = TransitionCreator.createMessage();
+        message.getMessageContent().setDocument(null);
+        DomibusConnectorMessage domainMessage = transformerService.transformTransitionToDomain(message);
+
+        assertThat(domainMessage).as("converted domainMessage must not be null!").isNotNull();
+        assertThat(domainMessage.getMessageDetails()).as("message details must not be null!").isNotNull();
+        assertThat(domainMessage.getMessageContent()).as("message content must not be null!").isNotNull();
+        assertThat(domainMessage.getMessageConfirmations()).as("message confirmations contains 1!").hasSize(1);
+        assertThat(domainMessage.getMessageErrors()).as("message errors contains 1!").hasSize(1);
+        assertThat(domainMessage.getMessageAttachments()).as("message attachments contains 1!").hasSize(1);
+    }
+
+    @Test
     public void testTransformTransitionToDomain_evidenceTriggerMessage() {
         DomibusConnectorMessageType msg = new DomibusConnectorMessageType();
         DomibusConnectorMessageDetailsType details = new DomibusConnectorMessageDetailsType();
