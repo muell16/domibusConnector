8b180e0b6ce3713bc7fb69f5df1a17f97421e489
Stephan Spindler <stephan.spindler@brz.gv.at>
2021-06-06 22:32:27 +0200

    remove old files from evidence toolkit integration

--- EvidencesModel/src/main/java/org/etsi/uri/_02231/v2/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.6 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2012.08.21 um 12:08:20 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://uri.etsi.org/02231/v2#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.etsi.uri._02231.v2;
--- EvidencesModel/src/main/java/org/w3/_2001/_04/xmlenc/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.6 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2012.08.21 um 12:08:20 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2001/04/xmlenc#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.w3._2001._04.xmlenc;
