fb07dfb7480ad3a217901c226d1a0242d4df7497
riederb <bernhard.rieder@brz.gv.at>
2021-10-28 14:09:15 +0200

    fix in mapping routing rules when property key not long enough

--- domibusConnector/domibusConnectorPersistence/src/main/java/eu/domibus/connector/persistence/service/impl/DCRoutingRulePersistenceServiceImpl.java
+++ domibusConnector/domibusConnectorPersistence/src/main/java/eu/domibus/connector/persistence/service/impl/DCRoutingRulePersistenceServiceImpl.java
@@ -94,6 +94,7 @@ public class DCRoutingRulePersistenceServiceImpl implements DCRoutingRulePersist
         Map<String, String> properties = messageLane.getProperties();
         Set<String> routingRuleIds = properties.keySet()
                 .stream()
+                .filter(k -> k.length()>PREFIX.length() && k.indexOf("]")>0)
                 .map(k -> k.substring(PREFIX.length()))
                 .map(k -> k.substring(0, k.indexOf("]")))
                 .collect(Collectors.toSet());
