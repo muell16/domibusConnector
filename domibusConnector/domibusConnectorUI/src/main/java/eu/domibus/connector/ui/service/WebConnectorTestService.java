52d8da863a8d80f4b11bd7ac0027f213866b3ebf
riederb <bernhard.rieder@brz.gv.at>
2021-10-29 08:15:00 +0200

    map WebMessage to transition model and submit to DCConnector2ConnectorTestService

--- domibusConnector/domibusConnectorUI/src/main/java/eu/domibus/connector/ui/service/WebConnectorTestService.java
+++ domibusConnector/domibusConnectorUI/src/main/java/eu/domibus/connector/ui/service/WebConnectorTestService.java
@@ -1,14 +1,34 @@
 package eu.domibus.connector.ui.service;
 
+import java.util.Optional;
+
+import javax.activation.DataHandler;
+import javax.xml.transform.Source;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
 import eu.domibus.connector.domain.configuration.EvidenceActionServiceConfigurationProperties.AS4Action;
 import eu.domibus.connector.domain.configuration.EvidenceActionServiceConfigurationProperties.AS4Service;
 import eu.domibus.connector.domain.model.DomibusConnectorBusinessDomain;
+import eu.domibus.connector.domain.transition.DomibusConnectorActionType;
+import eu.domibus.connector.domain.transition.DomibusConnectorDocumentAESType;
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageAttachmentType;
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageContentType;
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageDetailsType;
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageDocumentType;
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageType;
+import eu.domibus.connector.domain.transition.DomibusConnectorPartyType;
+import eu.domibus.connector.domain.transition.DomibusConnectorServiceType;
+import eu.domibus.connector.domain.transition.tools.ConversionTools;
 import eu.domibus.connector.test.service.DCConnector2ConnectorTestService;
 import eu.domibus.connector.ui.dto.WebMessage;
 import eu.domibus.connector.ui.dto.WebMessageDetail;
+import eu.domibus.connector.ui.dto.WebMessageDetail.Party;
+import eu.domibus.connector.ui.dto.WebMessageFile;
+import eu.domibus.connector.ui.dto.WebMessageFileType;
+import eu.europa.esig.dss.model.MimeType;
 
 
 @ConditionalOnBean(DCConnector2ConnectorTestService.class) 	//it might be possible, if the testbackend plugin is not enabled, that the service DCConnector2ConnectorTestService is not available!, in this case do not create WebConnectorTestService bean.
@@ -22,7 +42,9 @@ public class WebConnectorTestService {
 	}
 	
 	public void submitTestMessage(WebMessage testMsg) {
+		DomibusConnectorMessageType connectorMsg = mapWebMessageToTransition(testMsg);
 		
+		connectorTestService.submitTestMessage(connectorMsg);
 	}
 	
 	public WebMessageDetail.Service getTestService() {
@@ -39,6 +61,102 @@ public class WebConnectorTestService {
 	
 	public String getConnectorTestBackendName() {
 		return connectorTestService.getTestBackendName();
+	}
+	
+	public DomibusConnectorMessageType mapWebMessageToTransition(WebMessage webMsg) {
+		DomibusConnectorMessageType connectorMsg = new DomibusConnectorMessageType();
+		
+		DomibusConnectorMessageDetailsType details = mapDetails(webMsg.getMessageInfo());
+		
+		if(!StringUtils.isEmpty(webMsg.getConversationId()))
+			details.setConversationId(webMsg.getConversationId());
+		
+		connectorMsg.setMessageDetails(details);
+		
+		DomibusConnectorMessageContentType content = mapContent(webMsg);
+		connectorMsg.setMessageContent(content );
+		
+		webMsg.getFiles().stream()
+		.filter(f -> f.getFileType().equals(WebMessageFileType.BUSINESS_ATTACHMENT))
+		.forEach(a -> {
+			byte[] bytes = a.getFileContent();
+			DataHandler dh = ConversionTools.convertByteArrayToDataHandler(bytes, MimeType.BINARY.getMimeTypeString());
+			
+			DomibusConnectorMessageAttachmentType attachment = new DomibusConnectorMessageAttachmentType();
+			attachment.setAttachment(dh);
+			attachment.setName(a.getFileName());
+			attachment.setIdentifier(a.getFileName());
+			attachment.setMimeType(MimeType.BINARY.getMimeTypeString());
+			connectorMsg.getMessageAttachments().add(attachment );
+		});
+		
+		return connectorMsg;
+	}
+	
+	private DomibusConnectorMessageContentType mapContent(WebMessage webMsg) {
+		DomibusConnectorMessageContentType content = new DomibusConnectorMessageContentType();
+		
+		Optional<WebMessageFile> businessContent = webMsg.getFiles().stream()
+				.filter(f -> f.getFileType().equals(WebMessageFileType.BUSINESS_CONTENT))
+				.findFirst();
+		
+		if(businessContent.isPresent()) {
+			byte[] fileContent = businessContent.get().getFileContent();
+			Source streamSource = ConversionTools.convertByteArrayToStreamSource(fileContent);
+			content.setXmlContent(streamSource);
+		}
+		
+		Optional<WebMessageFile> businessDocument = webMsg.getFiles().stream()
+				.filter(f -> f.getFileType().equals(WebMessageFileType.BUSINESS_DOCUMENT))
+				.findFirst();
+		
+		if(businessDocument.isPresent()) {
+			byte[] fileContent = businessDocument.get().getFileContent();
+
+			DomibusConnectorMessageDocumentType document = new DomibusConnectorMessageDocumentType();
+			document.setDocumentName(businessDocument.get().getFileName());
+			
+			document.setAesType(DomibusConnectorDocumentAESType.AUTHENTICATION_BASED);
+			
+			DataHandler dh = ConversionTools.convertByteArrayToDataHandler(fileContent, MimeType.PDF.getMimeTypeString());
+			document.setDocument(dh );
+			
+			content.setDocument(document );
+		}
+		
+		
+		
+		return content;
+	}
+
+	private DomibusConnectorMessageDetailsType mapDetails(WebMessageDetail pDetails) {
+		DomibusConnectorMessageDetailsType details = new DomibusConnectorMessageDetailsType();
+		details.setFinalRecipient(pDetails.getFinalRecipient());
+		details.setOriginalSender(pDetails.getOriginalSender());
+		
+		Party from = pDetails.getFrom();
+		details.setFromParty(mapParty(from));
+		
+		Party to = pDetails.getTo();
+		details.setToParty(mapParty(to));
+		
+		DomibusConnectorActionType action = new DomibusConnectorActionType();
+		action.setAction(pDetails.getAction().getAction());
+		details.setAction(action);
+		
+		DomibusConnectorServiceType service = new DomibusConnectorServiceType();
+		service.setService(pDetails.getService().getService());
+		service.setServiceType(pDetails.getService().getServiceType());
+		details.setService(service);
+		
+		return details;
+	}
+	
+	private DomibusConnectorPartyType mapParty(Party pParty) {
+		DomibusConnectorPartyType cParty = new DomibusConnectorPartyType();
+		cParty.setPartyId(pParty.getPartyId());
+		cParty.setPartyIdType(pParty.getPartyIdType());
+		return cParty;
 	}
 
 }
