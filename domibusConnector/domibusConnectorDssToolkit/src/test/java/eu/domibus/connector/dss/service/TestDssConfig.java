d3222b2f85509e97eec5587fd8ee90d65f90d489
Stephan Spindler <stephan.spindler@brz.gv.at>
2021-12-06 13:40:13 +0100

    extended documentation on DSS config, LargeFileStorage

--- domibusConnector/domibusConnectorDssToolkit/src/test/java/eu/domibus/connector/dss/service/TestDssConfig.java
+++ domibusConnector/domibusConnectorDssToolkit/src/test/java/eu/domibus/connector/dss/service/TestDssConfig.java
@@ -1,14 +1,14 @@
 package eu.domibus.connector.dss.service;
 
 import eu.domibus.connector.common.SpringProfiles;
+import eu.domibus.connector.common.configuration.ConnectorConverterAutoConfiguration;
 import eu.domibus.connector.common.service.DCKeyStoreService;
 import eu.domibus.connector.dss.configuration.BasicDssConfiguration;
-import eu.ecodex.utils.spring.converter.ConverterAutoConfiguration;
 import eu.europa.esig.dss.enumerations.DigestAlgorithm;
 import eu.europa.esig.dss.model.TimestampBinary;
 import eu.europa.esig.dss.spi.DSSUtils;
 import eu.europa.esig.dss.spi.x509.tsp.TSPSource;
-import org.bouncycastle.jcajce.provider.digest.SHA512;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest(classes = {BasicDssConfiguration.class,
         DSSTrustedListsManager.class,
-        ConverterAutoConfiguration.class,
+        ConnectorConverterAutoConfiguration.class,
         DCKeyStoreService.class
 },
         properties = "connector.dss.tlCacheLocation=file:./target/tlcache/"
@@ -42,7 +39,7 @@ public class TestDssConfig {
         final byte[] digestValue = DSSUtils.digest(digestAlgorithm, toDigest);
 
         TimestampBinary timeStampResponse = tspSource.getTimeStampResponse(digestAlgorithm, digestValue);
-        assertThat(timeStampResponse.getBytes()).isNotNull();
+        AssertionsForClassTypes.assertThat(timeStampResponse.getBytes()).isNotNull();
     }
 
 }
