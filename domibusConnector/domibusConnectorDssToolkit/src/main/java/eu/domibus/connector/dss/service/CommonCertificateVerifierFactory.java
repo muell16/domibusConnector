9e6ba4c121b77ae300285324b08a3da9d2aebd0e
Stephan Spindler <stephan.spindler@brz.gv.at>
2021-12-06 19:18:20 +0100

    fix AIA data loader, so it also uses ProxyConfig

--- domibusConnector/domibusConnectorDssToolkit/src/main/java/eu/domibus/connector/dss/service/CommonCertificateVerifierFactory.java
+++ domibusConnector/domibusConnectorDssToolkit/src/main/java/eu/domibus/connector/dss/service/CommonCertificateVerifierFactory.java
@@ -1,18 +1,26 @@
 package eu.domibus.connector.dss.service;
 
 import eu.domibus.connector.dss.configuration.CertificateVerifierConfigurationProperties;
+import eu.europa.esig.dss.model.x509.CertificateToken;
+import eu.europa.esig.dss.model.x509.revocation.crl.CRL;
 import eu.europa.esig.dss.service.crl.OnlineCRLSource;
 import eu.europa.esig.dss.service.ocsp.OnlineOCSPSource;
+import eu.europa.esig.dss.spi.client.http.DataLoader;
 import eu.europa.esig.dss.spi.tsl.TrustedListsCertificateSource;
 import eu.europa.esig.dss.spi.x509.CertificateSource;
 import eu.europa.esig.dss.spi.x509.CommonTrustedCertificateSource;
 import eu.europa.esig.dss.spi.x509.ListCertificateSource;
+import eu.europa.esig.dss.spi.x509.revocation.RevocationSource;
+import eu.europa.esig.dss.spi.x509.revocation.RevocationToken;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static eu.domibus.connector.dss.configuration.BasicDssConfiguration.DEFAULT_DATALOADER_BEAN_NAME;
 
 @Service
 public class CommonCertificateVerifierFactory {
@@ -23,12 +31,16 @@ public class CommonCertificateVerifierFactory {
     private final OnlineOCSPSource onlineOCSPSource;
     private final CertificateSourceFromKeyStoreCreator certificateSourceFromKeyStoreCreator;
     private final DSSTrustedListsManager trustedListsManager;
+    private final DataLoader dataLoader;
 
-    public CommonCertificateVerifierFactory(OnlineCRLSource onlineCRLSource,
-                                            OnlineOCSPSource onlineOCSPSource,
-                                            CertificateSourceFromKeyStoreCreator certificateSourceFromKeyStoreCreator,
-                                            DSSTrustedListsManager trustedListsManager) {
-
+    public CommonCertificateVerifierFactory(
+            @Qualifier(DEFAULT_DATALOADER_BEAN_NAME) DataLoader defaultDataLoader,
+            OnlineCRLSource onlineCRLSource,
+            OnlineOCSPSource onlineOCSPSource,
+            CertificateSourceFromKeyStoreCreator certificateSourceFromKeyStoreCreator,
+            DSSTrustedListsManager trustedListsManager
+    ) {
+        this.dataLoader = defaultDataLoader;
         this.onlineCRLSource = onlineCRLSource;
         this.onlineOCSPSource = onlineOCSPSource;
         this.certificateSourceFromKeyStoreCreator = certificateSourceFromKeyStoreCreator;
@@ -39,17 +51,21 @@ public class CommonCertificateVerifierFactory {
         CommonCertificateVerifier commonCertificateVerifier = new CommonCertificateVerifier();
         ListCertificateSource listCertificateSource = new ListCertificateSource();
 
+        commonCertificateVerifier.setDataLoader(dataLoader);
+
         if (certificateVerifierConfig.isOcspEnabled()) {
             commonCertificateVerifier.setOcspSource(onlineOCSPSource);
             LOGGER.debug("OCSP checking is enabled");
         } else {
             LOGGER.debug("OCSP checking is NOT enabled");
+            commonCertificateVerifier.setOcspSource(null);
         }
         if (certificateVerifierConfig.isCrlEnabled()) {
             LOGGER.debug("CRL checking is enabled");
             commonCertificateVerifier.setCrlSource(onlineCRLSource);
         } else {
             LOGGER.debug("CRL checking is NOT enabled");
+            commonCertificateVerifier.setCrlSource(null);
         }
 
         String trustedListSourceName = certificateVerifierConfig.getTrustedListSource();
