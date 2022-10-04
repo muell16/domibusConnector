d4ecba95d1d1be9b25fbb4a17c845822828b3a46
weinek <bruno.weinek@extern.brz.gv.at>
2022-10-04 21:15:17 +0200

    adds testcontainers.

---
+++ domibusConnector/dc5Model/src/test/java/eu/dc5/domain/TestApplication.java
@@ -0,0 +1,7 @@
+package eu.dc5.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+}
---
+++ domibusConnector/dc5Model/src/test/java/eu/dc5/domain/model/DC5MessageTest.java
@@ -0,0 +1,28 @@
+package eu.dc5.domain.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class DC5MessageTest {
+    @Container
+    static MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:latest");
+
+    @DynamicPropertySource
+    static void registerDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mariadb::getJdbcUrl);
+        registry.add("spring.datasource.username", mariadb::getUsername);
+        registry.add("spring.datasource.password", mariadb::getPassword);
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
