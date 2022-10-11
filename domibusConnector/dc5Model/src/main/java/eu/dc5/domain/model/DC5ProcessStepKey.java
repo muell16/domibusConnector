bbc3964dd17fa25d7a09c4e54918b733a71e0868
weinek <bruno.weinek@extern.brz.gv.at>
2022-10-11 07:45:24 +0200

    rename file.

---
+++ domibusConnector/dc5Model/src/main/java/eu/dc5/domain/model/DC5ProcessStepKey.java
@@ -0,0 +1,6 @@
+package eu.dc5.domain.model;
+
+public enum DC5ProcessStepKey {
+    // TODO: keys for EnumMap
+    FOO
+}
--- domibusConnector/dc5Model/src/main/java/eu/dc5/domain/model/DC5_Process_Step_Key.java
+++
@@ -1,5 +0,0 @@
-package eu.dc5.domain.model;
-
-public enum DC5_Process_Step_Key {
-    FOO
-}
