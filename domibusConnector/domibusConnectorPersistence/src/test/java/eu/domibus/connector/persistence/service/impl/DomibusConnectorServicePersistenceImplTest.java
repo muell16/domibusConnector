637595dbe33556a0d64e244e520981d76db72e74
Stephan Spindler <stephan.spindler@brz.gv.at>
2022-12-28 12:45:38 +0100

    cleanup tests

--- domibusConnector/domibusConnectorPersistence/src/test/java/eu/domibus/connector/persistence/service/impl/DomibusConnectorServicePersistenceImplTest.java
+++ domibusConnector/domibusConnectorPersistence/src/test/java/eu/domibus/connector/persistence/service/impl/DomibusConnectorServicePersistenceImplTest.java
@@ -1,16 +1,6 @@
 package eu.domibus.connector.persistence.service.impl;
 
-import eu.domibus.connector.persistence.dao.DomibusConnectorServiceDao;
-import eu.domibus.connector.persistence.model.test.util.PersistenceEntityCreator;
-import eu.domibus.connector.persistence.service.DomibusConnectorServicePersistenceService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
