078c856d01c642e412a13ce59b718adbbab8711b
Stephan Spindler <stephan.spindler@brz.gv.at>
2022-12-28 17:20:57 +0100

    add work on process state

--- domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/process/model/DC5ProcessStateEventHandler.java
+++ domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/process/model/DC5ProcessStateEventHandler.java
@@ -13,10 +13,9 @@ public class DC5ProcessStateEventHandler {
     private final DC5MsgProcessRepo msgProcessRepo;
 
     @EventListener
-    public void DC5ProcessEvent(DC5MessageProcessEvent event) {
-        msgProcessRepo.getDC5MsgProcessByProcessId(event.getProcessId());
-
-
+    public void handleDC5ProcessEvent(DC5MessageProcessEvent event) {
+        DC5MsgProcess dc5MsgProcessByProcessId = msgProcessRepo.getDC5MsgProcessByProcessId(event.getProcessId());
+        //load state machine for process dc5MsgProcessByProcessId and publish state to it
     }
 
 
--- domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/process/model/DC5ProcessStateMachineFactory.java
+++ domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/process/model/DC5ProcessStateMachineFactory.java
@@ -3,11 +3,17 @@ package eu.ecodex.dc5.process.model;
 import eu.domibus.connector.controller.exception.DCEvidenceProcessingException;
 import eu.domibus.connector.controller.exception.ErrorCode;
 import eu.ecodex.dc5.message.model.DC5BusinessMessageState;
+import eu.ecodex.dc5.message.model.DC5Confirmation;
 import eu.ecodex.dc5.process.repository.DC5MsgProcessRepo;
 import eu.ecodex.dc5.statemachine.StateTransitionChecker;
+import lombok.AccessLevel;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static eu.ecodex.dc5.message.model.DC5BusinessMessageState.BusinessMessageEvents.*;
 import static eu.ecodex.dc5.message.model.DC5BusinessMessageState.BusinessMessageEvents.ADMIN_ABORT;
@@ -24,6 +30,35 @@ public class DC5ProcessStateMachineFactory {
                 .transition(DC5ProcessState.NEW, DC5ProcessState.SAVED, DC5ProcessEvents.SAVED)
 
                 .build();
+    }
+
+    public DC5ProcessStateMachine loadStateMachine(DC5ProcessStateItem state) {
+        return new DC5ProcessStateMachine(state);
+    }
+
+    public static class DC5ProcessStateItem {
+
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public class DC5ProcessStateMachine {
+
+        @NonNull
+        private final DC5ProcessStateItem currentState;
+
+        public void publishEvent(DC5ProcessEvents event) {
+//            Optional<DC5BusinessMessageState.BusinessMessagesStates> state = stateTransitionChecker.doTransition(currentState.getState(), event);
+//            if (state.isPresent()) {
+//                return DC5BusinessMessageState.builder()
+//                        .state(state.get())
+//                        .created(LocalDateTime.now())
+//                        .event(event)
+//                        .confirmation(dc5Confirmation)
+//                        .build();
+//            }
+//            throw new IllegalStateException(String.format("The transition from [%s] to [%s] is not possible!", state, event));
+        }
+
     }
 
 }
