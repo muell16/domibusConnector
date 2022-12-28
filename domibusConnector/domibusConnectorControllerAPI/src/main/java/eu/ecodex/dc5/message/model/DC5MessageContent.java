ed825701e89d01c13ad6b831865fb564a6b12555
Stephan Spindler <stephan.spindler@brz.gv.at>
2022-12-28 07:25:08 +0100

    fix compile error

--- domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/message/model/DC5MessageContent.java
+++ domibusConnector/domibusConnectorControllerAPI/src/main/java/eu/ecodex/dc5/message/model/DC5MessageContent.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 
