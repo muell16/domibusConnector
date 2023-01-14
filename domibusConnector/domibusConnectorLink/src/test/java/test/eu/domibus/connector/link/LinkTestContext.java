09e32cef1e35e1aca31dc6ee08687a64db5bf7c2
Stephan Spindler <stephan.spindler@brz.gv.at>
2023-01-14 14:05:50 +0100

    repair test

--- domibusConnector/domibusConnectorLink/src/test/java/test/eu/domibus/connector/link/LinkTestContext.java
+++ domibusConnector/domibusConnectorLink/src/test/java/test/eu/domibus/connector/link/LinkTestContext.java
@@ -3,9 +3,14 @@ package test.eu.domibus.connector.link;
 import eu.domibus.connector.common.configuration.ConnectorConfigurationProperties;
 import eu.domibus.connector.common.ConfigurationPropertyManagerServiceImpl;
 import eu.domibus.connector.common.service.DCKeyStoreService;
+import eu.domibus.connector.common.service.PropertyMapToBeanConverter;
+import eu.domibus.connector.controller.exception.ErrorCode;
 import eu.domibus.connector.controller.routing.DCRoutingRulesManager;
 import eu.domibus.connector.controller.service.DomibusConnectorMessageIdGenerator;
+import eu.domibus.connector.controller.service.SubmitToConnector;
 import eu.domibus.connector.controller.service.TransportStateService;
+import eu.domibus.connector.domain.testutil.DomainEntityCreator;
+import eu.ecodex.dc5.domain.DCBusinessDomainManager;
 import eu.ecodex.dc5.message.model.DC5Message;
 import eu.ecodex.dc5.message.model.DC5MessageId;
 import eu.domibus.connector.domain.transformer.DomibusConnectorDomainMessageTransformerService;
@@ -18,6 +23,9 @@ import eu.domibus.connector.persistence.service.DCLinkPersistenceService;
 import eu.domibus.connector.persistence.service.DCMessagePersistenceService;
 import eu.domibus.connector.persistence.service.LargeFilePersistenceService;
 import eu.domibus.connector.persistence.service.testutil.LargeFilePersistenceServicePassthroughImpl;
+import eu.ecodex.dc5.process.MessageProcessId;
+import eu.ecodex.dc5.process.model.DC5MsgProcess;
+import eu.ecodex.dc5.transport.repo.DC5TransportRequestRepo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mockito.Mockito;
@@ -32,23 +40,48 @@ import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, }, scanBasePackages = "eu.domibus.connector.link")
 @Import({LinkTestContext.LinkServiceContext.class,
         HelperMethods.class,
-        DCKeyStoreService.class,
-        ConfigurationPropertyManagerServiceImpl.class,
-//        DCBusinessDomainManagerImpl.class,
+//        DCKeyStoreService.class,
+//        ConfigurationPropertyManagerServiceImpl.class,
+//        PropertyMapToBeanConverter.class,
+
+        //        DCBusinessDomainManagerImpl.class,
         ConnectorConfigurationProperties.class
 })
-@ComponentScan(basePackages = {"eu.domibus.connector.common", "eu.domibus.connector"})
+@ComponentScan(basePackages = {"eu.domibus.connector.common"})
 public class LinkTestContext {
 
     private static final Logger LOGGER = LogManager.getLogger(LinkTestContext.class);
 
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public MerlinPropertiesFactory merlinPropertiesFactory() {
+//        return new MerlinPropertiesFactory();
+//    }
+
+//    @MockBean
+//    SubmitToConnector submitToConnector;
+
     @Bean
-    public MerlinPropertiesFactory merlinPropertiesFactory() {
-        return new MerlinPropertiesFactory();
+    public SubmitToConnector submitToConnectorMockBean(@Qualifier(SUBMIT_TO_CONNECTOR_QUEUE) BlockingQueue<DC5Message> toConnectorMessages) {
+        SubmitToConnector submitToConnector = new SubmitToConnector() {
+            @Override
+            public <T> ReceiveMessageFlowResult receiveMessage(T message, DC5TransformToDomain<T> transform) {
+                DC5Message dc5Msg = transform.transform(message, DC5MsgProcess.builder().processId(MessageProcessId.ofRandom()).build());
+                toConnectorMessages.add(dc5Msg);
+                return ReceiveMessageFlowResult.getSuccess(dc5Msg);
+            }
+        };
+        return submitToConnector;
     }
+
+    @MockBean
+    DC5TransportRequestRepo dc5TransportRequestRepo;
+
+    @MockBean
+    DCBusinessDomainManager dcBusinessDomainManager;
 
     @MockBean
     DomibusConnectorLinkPartnerDao dao;
