4cce2d87296ed2bfc7b9ed74a19fb6efe97e9f46
weinek <bruno.weinek@extern.brz.gv.at>
2023-01-24 11:37:13 +0100

    add assertion to test.

--- domibusConnector/domibusConnectorController/src/test/java/eu/ecodex/dc5/message/FindBusinessMessageByMsgIdTest.java
+++ domibusConnector/domibusConnectorController/src/test/java/eu/ecodex/dc5/message/FindBusinessMessageByMsgIdTest.java
@@ -2,6 +2,7 @@ package eu.ecodex.dc5.message;
 
 import eu.domibus.connector.controller.service.SubmitToLinkService;
 import eu.ecodex.dc5.flow.flows.FlowTestAnnotation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +19,6 @@ class FindBusinessMessageByMsgIdTest {
 
         String conversationId = "testConvId";
 
-        findBusinessMessageByMsgId.findBusinessMsgByConversationId(conversationId);
+        Assertions.assertDoesNotThrow(()->findBusinessMessageByMsgId.findBusinessMsgByConversationId(conversationId));
     }
 }
